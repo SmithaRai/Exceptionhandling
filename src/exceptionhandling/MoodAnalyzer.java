ab6ba656a76101f52f5ea39e2678682aee98c4df
unknown <SmithaRai.com>
2022-04-22 16:03:23 +0530

    Added : added analysemood  method

--- src/exceptionhandling/MoodAnalyzer.java
+++ src/exceptionhandling/MoodAnalyzer.java
@@ -1,9 +1,20 @@
 package exceptionhandling;
 
 public class MoodAnalyzer {
+	
+	void analysemood(String message)
+	{
+		if(message == "Happy")
+			System.out.println("happy mood");
+		else
+			System.out.println("sad mood");
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*CUSTOM EXCEPTION HANDLING-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* ");
+		System.out.println(" ");
+		MoodAnalyzer mood = new MoodAnalyzer();
+		 mood.analysemood("Happy");
 	}
 }
